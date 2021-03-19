74c65351813e74a6e9655fbfb088bf7a245070f8
yusufsobad <yusuf_soloabadi@yahoo.co.id>
2021-03-19 17:11:55 +0700

    Fix Bug Empoyee Report

--- pages/Dashboard/view/Admin/employeeReport.php
+++ pages/Dashboard/view/Admin/employeeReport.php
@@ -386,7 +386,6 @@ class employeeReport_absen extends _page{
 
 	public function _layout(){
 		?>
-			<?xml version="1.0" encoding="UTF-8"?>
 			<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 
 			<div id="report-employee">
